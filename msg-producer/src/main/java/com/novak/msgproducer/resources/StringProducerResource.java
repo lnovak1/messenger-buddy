812a8e52e9b1c95df48c4f791ff2e430f2c78ee0
lnovak1 <lnvk98@gmail.com>
2023-06-16 20:11:15 -0300

    kafka producer with simple string and docker

---
+++ msg-producer/src/main/java/com/novak/msgproducer/resources/StringProducerResource.java
@@ -0,0 +1,22 @@
+package com.novak.msgproducer.resources;
+
+import com.novak.msgproducer.services.StringProducerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/producer")
+public class StringProducerResource {
+
+    private final StringProducerService producerService;
+    @PostMapping
+    public ResponseEntity<?> sendMessage(@RequestBody String message){
+    producerService.sendMessage(message);
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
