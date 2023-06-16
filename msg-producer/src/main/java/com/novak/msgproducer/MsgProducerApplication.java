b3f3bef76b090f284ecc1f3ecc9289dd3997332d
lnovak1 <lnvk98@gmail.com>
2023-06-16 19:20:05 -0300

    msg-producer bootstrap

---
+++ msg-producer/src/main/java/com/novak/msgproducer/MsgProducerApplication.java
@@ -0,0 +1,13 @@
+package com.novak.msgproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgProducerApplication.class, args);
+	}
+
+}
