be9b844ba75d26d30da5290bf1e7b02c1e860297
lnovak1 <lnvk98@gmail.com>
2023-06-16 21:32:13 -0300

    added callback to check event producer

--- msg-producer/src/main/java/com/novak/msgproducer/services/StringProducerService.java
+++ msg-producer/src/main/java/com/novak/msgproducer/services/StringProducerService.java
@@ -1,8 +1,11 @@
 package com.novak.msgproducer.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class StringProducerService {
@@ -11,6 +14,18 @@ public class StringProducerService {
     private final KafkaTemplate<String,String> kafkaTemplate;
 
     public void sendMessage(String message){
-        kafkaTemplate.send("str-topic",message);
+       var future =  kafkaTemplate.send("str-topic",message);
+       future.whenComplete((result,exception) ->{
+          if (result != null){
+              log.info("Send message with success ! message: {},partition {}, offset{}",
+                      message,
+                      result.getRecordMetadata().partition(),
+                      result.getRecordMetadata().offset());
+          }
+          if(exception!= null){
+              log.error("Error while sending message {}", exception.getStackTrace());
+          }
+       });
+
     }
 }
