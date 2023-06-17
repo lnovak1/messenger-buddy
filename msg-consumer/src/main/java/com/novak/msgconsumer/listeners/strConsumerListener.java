1c978a4e01f05a4fec3d582436d3132cdb854bd4
lnovak1 <lnvk98@gmail.com>
2023-06-17 12:01:15 -0300

    refactor: reduce consumer listener complexity with custom annotation

--- msg-consumer/src/main/java/com/novak/msgconsumer/listeners/strConsumerListener.java
+++ msg-consumer/src/main/java/com/novak/msgconsumer/listeners/strConsumerListener.java
@@ -1,5 +1,6 @@
 package com.novak.msgconsumer.listeners;
 
+import com.novak.msgconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class strConsumerListener {
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void listener(String message){
         log.info("received message : {}", message);
     }
